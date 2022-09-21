dd634489a001f84d9283a4c7b0b82f284551a9ad
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-09-21 04:55:22 -0400

    Wrote a solution using unordered_set, doesn't count frequency

---
+++ Week_2/Accelerated_CPP_4_5/app/main.cpp
@@ -0,0 +1,51 @@
+/**
+ * @copyright Copyright (c) 2022
+ *
+ * @file main.cpp
+ * @author Tanuj Thakkar (tanuj@umd.edu)
+ * @version 0.1
+ * @date 2022-09-21
+ * 
+ * @brief ENPM808X, Week 2, Accelerated C++ - 4-5
+ 
+ * @section DESCRIPTION
+ * 
+ * This program reads an input stream and counts the repetitions for each word
+ * in the recorded stream, as well as the total number of words in the input 
+ * stream.
+ */
+
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <vector>
+#include <unordered_set>
+
+
+void processInputStream(std::istream& in, std::vector<std::string>& words, std::unordered_set<std::string>& frequencyCounts) {
+
+    std::string word;
+    while (in >> word) {
+        words.push_back(word);
+        frequencyCounts.insert(word);
+    }
+}
+
+
+int main()
+{   
+    std::vector<std::string> words;
+    std::unordered_set<std::string> frequencyCounts;
+
+    std::cout << "Enter your desired text input, use Ctrl+D to end" << std::endl;
+    processInputStream(std::cin, words, frequencyCounts);
+
+    int totalWordCount = 0;
+    for (size_t i = 0; i < words.size(); ++i) {
+        std::cout << words[i] << "\t" << frequencyCounts.count(words[i]) << std::endl;
+        totalWordCount += frequencyCounts.count(words[i]);
+    }
+    std::cout << "Total Words: " << totalWordCount << std::endl;
+    
+    return 0;
+}
