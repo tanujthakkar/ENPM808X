7e6427f72dc398bb5d5eb22adf1e9be902871e17
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-09-21 16:03:40 -0400

    Updated format using clang, cpplint passing

--- Week_2/Software_Engineering_3_10/include/course.hpp
+++ Week_2/Software_Engineering_3_10/include/course.hpp
@@ -18,24 +18,26 @@
 #define WEEK_2_SOFTWARE_ENGINEERING_3_10_INCLUDE_COURSE_HPP_
 
 #include <iostream>
+#include <numeric>
 #include <vector>
-
+#include <string>
 
 class Course {
-    private:
-        std::string name_;  // String to store course name
-        int totalStudents_;  // Int to store total students in the course
-        std::vector<int> studentNos_;  // Vector to store student numbers
-        std::vector<double> studentGrades;  // Vector to store student grades
-    
-    public:
-        Course();  // Default constructor
-        ~Course();  // Default destructor
-    
-        void getStudentGrade(const int& studentNo);
-        void updateStudentGrade(const int& studentNo, const double& newGrade);
-        double calcCourseAvg();
-        void displayCourseSummary();
+ private:
+  std::string name_;             // String to store course name
+  int totalStudents_;            // Int to store total students in the course
+  std::vector<int> studentNos_;  // Vector to store student numbers
+  std::vector<double> studentGrades_;  // Vector to store student grades
+
+ public:
+  Course(const std::string& courseName,
+         const int& totalStudents);  // Default constructor
+  ~Course();                         // Default destructor
+
+  double getStudentGrade(const int& studentNo);
+  void updateStudentGrade(const int& studentNo, const double& newGrade);
+  double calcCourseAvg();
+  void displayCourseSummary();
 };
 
-#endif
+#endif  // WEEK_2_SOFTWARE_ENGINEERING_3_10_INCLUDE_COURSE_HPP_
