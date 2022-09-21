37088ae0cfa8d52b6c2cc37bfdcc877fdadeb884
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-09-21 00:47:21 -0400

    Created a new branch, boilerplate_vanilla, for a vanilla version of cpp_boilerplate

---
+++ Week_2/cpp_boilerplate/build/app/CMakeFiles/shell-app.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tanujthakkar/UMD/3_Fall_22/ENPM808X/Week_2/Accelerated_CPP_3_4/app/main.cpp" "/home/tanujthakkar/UMD/3_Fall_22/ENPM808X/Week_2/Accelerated_CPP_3_4/build/app/CMakeFiles/shell-app.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Week_2/cpp_boilerplate/build/test/CMakeFiles/cpp-test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tanujthakkar/UMD/3_Fall_22/ENPM808X/Week_2/Accelerated_CPP_3_4/test/main.cpp" "/home/tanujthakkar/UMD/3_Fall_22/ENPM808X/Week_2/Accelerated_CPP_3_4/build/test/CMakeFiles/cpp-test.dir/main.cpp.o"
+  "/home/tanujthakkar/UMD/3_Fall_22/ENPM808X/Week_2/Accelerated_CPP_3_4/test/test.cpp" "/home/tanujthakkar/UMD/3_Fall_22/ENPM808X/Week_2/Accelerated_CPP_3_4/build/test/CMakeFiles/cpp-test.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
